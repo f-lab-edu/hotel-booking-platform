b1273ef04df216d01788e7a12e0e979e0b824cbb
muho <mh.jung.dev@gmail.com>
2025-10-10 14:13:24 +0900

    feat: hotel-service Availability API 비동기로 변환

--- service/hotel-service/src/main/java/dev/muho/hotel/repository/RoomTypeRepository.java
+++ service/hotel-service/src/main/java/dev/muho/hotel/repository/RoomTypeRepository.java
@@ -1,16 +1,12 @@
 package dev.muho.hotel.repository;
 
-import dev.muho.hotel.domain.Hotel;
 import dev.muho.hotel.domain.RoomType;
 import dev.muho.hotel.domain.Status;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface RoomTypeRepository extends JpaRepository<RoomType, Long> {
-    List<RoomType> findByHotel(Hotel hotel);
     Page<RoomType> findByHotelId(Long hotelId, Pageable pageable);
 
     // 경우의 수별 검색 메서드들
